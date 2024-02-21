1d2acbd6344d912074e459f704faf046f75e825f
vanstevanzaky <143470195+vanstevanzaky@users.noreply.github.com>
2024-02-21 16:42:29 +0700

    Second Commit

--- Week 2/BukuMain.java
+++
@@ -1,7 +0,0 @@
-
-public static void main(String[] args) {
-        Buku bk1 = new Buku();
-        bk1.judul = "Today Ends Tommorrow Comes";
-        bk1.pengarang = "Denanda Pratiwi";
-        bk1.
-}
